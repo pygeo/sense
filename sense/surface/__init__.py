fdeb713e18589bebdb9f6a8640dd10f7f15dc320
Alex <alexander.loew@lmu.de>
2017-05-30 21:26:55 +0200

    inital iem2

--- sense/surface/__init__.py
+++ sense/surface/__init__.py
@@ -1,1 +1,2 @@
 from . oh1992 import Oh92
+from . i2em import *
---
+++ sense/surface/i2em.py
@@ -0,0 +1,28 @@
+"""
+implements the I2EM model (see Ulaby (2014), Chapter 10
+backscattering model for single scale random surfaces
+"""
+
+
+class I2EM(object):
+    def __init__(self, eps, ks, theta):
+        """
+        Parameters
+        ----------
+        eps : complex
+            relative dielectric permitivity
+        ks : float
+            vertical surface roughness
+        theta : float
+            incidence angle [rad]
+        """
+        self._calc_sigma_backscatter()
+
+    def _calc_sigma_backscatter(self):
+        # define backscatter geometry
+        theta_s = self.theta*1.
+        phi_s = np.deg2rad(180.)
+
+        # calculate backscattering coefficients
+        self.vv, self.hh = self.i2em_bistatic()
+        self.hv = self.i2em_cross()
