6aa425c9f8a5c0e3feea741956b52e4835b73e77
Alex <alexander.loew@lmu.de>
2017-05-31 07:56:59 +0200

    added dubois model and example figure plot

--- sense/surface/dubois95.py
+++ sense/surface/dubois95.py
@@ -28,7 +28,12 @@ class Dubois95(object):
         lam : float
             wavelength in cm
         """
-        return 10**(-2.75)*((np.cos(self.theta)**1.5)/np.sin(self.theta)**5.)*(10.**(0.028*np.real(self.eps))*np.tan(self.theta))*(ks*np.sin(self.theta)**1.4*lam**0.7)  # eq. 10.41a0
+
+        a = (10.**-2.75)*(np.cos(self.theta)**1.5)/(np.sin(self.theta)**5.)
+        c = 10.**(0.028*np.real(self.eps)*np.tan(self.theta))
+        d = ((ks*np.sin(self.theta))**1.4)*lam**0.7
+
+        return a*c*d
 
     def _vv(self, lam, ks):
         """ eq. 10.41b """
