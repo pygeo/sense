0f1c3cf51f2e1be6d9c057b5feb1c0ced723a321
Alex <alexander.loew@lmu.de>
2017-06-01 17:45:09 +0200

    initialization of IEM working ...

--- sense/surface/i2em.py
+++ sense/surface/i2em.py
@@ -1,24 +1,96 @@
 """
 implements the I2EM model (see Ulaby (2014), Chapter 10
 backscattering model for single scale random surfaces
+
+
+The code originates from ideas obtained from the supplement
+of Ulaby et al (2014)
 """
 from scatter import SurfaceScatter
 import numpy as np
 
+from .. util import f2lam
+import math
+
 class I2EM(SurfaceScatter):
-    def __init__(self, eps, ks, kl, theta):
+    def __init__(self, f, eps, s, l, theta, thetas, phis):
         """
         Parameters
         ----------
+        f : float
+            frequency [GHz]
         eps : complex
             relative dielectric permitivity
-        ks : float
-            vertical surface roughness
+        s : float
+            vertical surface roughness  [m]
+        l : float
+            autocorrelation length [m]
         theta : float
             incidence angle [rad]
+        thetas : float
+            scattering angle [rad]
+        phis : float
+            azimuth angle for scattering [rad]
         """
-        super(I2EM, self).__init__(eps, ks, theta, kl=kl)
+
+        self.phi = 0.
+        self.phis = phis
+
+        self.freq = f
+        lam = f2lam(self.freq)
+        k = 2.*np.pi/lam
+        self.k = k
+        self.s = s
+        self.l = l
+        super(I2EM, self).__init__(eps, k*s, theta, kl=k*l)
+        self.thetas = thetas
+        
+        # do initializations for backscatter calculations
+        self._init_hlp()
+        self.init_model()
+
+        # calculate the actual backscattering coefficients
         self._calc_sigma_backscatter()
+
+    def init_model(self):
+        """
+        initialize model for calculations
+        """
+        self.niter = self._estimate_itterations()
+
+    def _estimate_itterations(self):
+        """
+        estimate the number of necessary itterations for 
+        the integral calculations
+        """
+
+        err = 1.E8
+        Ts = 1
+        while err > 1.0e-8:
+            Ts += 1
+            err = ((self._ks2 *(self._cs + self._css)**2 )**Ts) / math.factorial(Ts)
+        return Ts
+
+
+    def _init_hlp(self):
+        """ initiate help variables """
+        self._ks2 = self.ks**2.
+        self._cs = np.cos(self.theta)
+        self._s = np.sin(self.theta)
+        self._sf = np.sin(self.phi)
+        self._cf = np.cos(self.phi)
+        self._ss = np.sin(self.thetas)
+        self._css = np.cos(self.thetas)
+        self._cfs = np.cos(self.phis)
+        self._sfs = np.sin(self.phis)
+        self._s2 = self.s**2.
+        self._kx = self.k*self.s*self._cf
+        self._ky = self.k*self.s*self._sf
+        self._kz = self.k*self._cs
+
+        self._ksx = self.k * self._ss *self._cfs
+        self._ksy = self.k * self._ss *self._sfs
+        self._ksz = self.k * self._css
 
     def _calc_sigma_backscatter(self):
         # define backscatter geometry
--- tests/test_iem.py
+++ tests/test_iem.py
@@ -4,22 +4,26 @@ import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + os.sep + "..")
 
 from sense.surface import I2EM
+import numpy as np
 
 class Test_IEM(unittest.TestCase):
     def setUp(self):
         self.eps = 4.+0.j
-        self.ks = 1.
         self.theta = 0.5
-        self.kl = 2.
+        self.f = 6.
+        self.s = 0.01
+        self.l = 0.1
+        self.thetas = 0.5
+        self.phis = np.pi
 
     def tearDown(self):
         pass
     
     def test_init(self):
-        O = I2EM(self.eps, self.ks, self.kl, self.theta)
+        O = I2EM(self.f, self.eps, self.s, self.l, self.theta, self.thetas, self.phis)
         self.assertEqual(O.eps, self.eps)
-        self.assertEqual(O.ks, self.ks)
-        self.assertEqual(O.kl, self.kl)
+        self.assertEqual(O.ks, O.k*O.s)
+        self.assertEqual(O.kl, O.k*O.l)
 
 
 if __name__ == '__main__':
